28ef4a3d8da66bceead81e6bc36caa68beb0754a
James Boer <boer.james@gmail.com>
2017-04-08 16:34:04 -0600

    books filtering and layout

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,26 +1,86 @@
 import React, {Component} from 'react'
 
 import Book from './Book.jsx'
-import Filter from './Filter.jsx'
 
 import data from '../data/books.json'
 
 class Books extends Component {
     constructor(props) {
         super(props)
-        this.books = data.books
+        this.state = {
+            books: data.books,
+            authors: data.authors,
+            categories: data.categories
+        }
+        this.updateFilter = this.updateFilter.bind(this)
+    }
+
+    updateFilter() {
+        const titleValue = document.getElementById('title-filter').value
+        const authorValue = document.getElementById('author-filter').value
+        const categoryValue = document.getElementById('category-filter').value
+        
+        let filteredBooks = data.books.filter(book => {
+            if (book.title.toLowerCase().includes(titleValue)) return true
+            return false
+        })
+
+        filteredBooks = filteredBooks.filter(book => {
+            for (let author of book.authors) if (author.includes(authorValue)) return true
+            return false
+        })
+
+        filteredBooks = filteredBooks.filter(book => {
+            for (let category of book.categories) if (category.includes(categoryValue)) return true
+            return false
+        })
+
+        this.setState({
+            books: filteredBooks
+        })
     }
 
     render() {
         return (
             <div className="container">
-                <div className="columns">
+                <div className="columns" style={{padding: '15px'}}>
                     <div className="column is-4">
-                        <Filter/>
+
+                        <div className="field">
+                            <label className="label">Title</label>
+                            <p className="control">
+                                <input className="input" type="text" id="title-filter" onChange={this.updateFilter}/>
+                            </p>
+                        </div>
+
+                        <div className="field">
+                            <label className="label">Author</label>
+                            <p className="control">
+                                <span className="select" style={{width:'100%'}}>
+                                    <select id="author-filter" onChange={this.updateFilter} style={{width:'100%'}}>
+                                        <option value="">Select Author</option>
+                                        {this.state.authors.map(author => (<option value={author}>{author}</option>))}
+                                    </select>
+                                </span>
+                            </p>
+                        </div>
+
+                        <div className="field">
+                            <label className="label">Category</label>
+                            <p className="control">
+                                <span className="select" style={{width:'100%'}}>
+                                    <select id="category-filter" onChange={this.updateFilter} style={{width:'100%'}}>
+                                        <option value="">Select Category</option>
+                                        {this.state.categories.map(category => (<option value={category}>{category}</option>))}
+                                    </select>
+                                </span>
+                            </p>
+                        </div>
+
                     </div>
                     <div className="column is-8">
-                        <div className="card" style={{height:'88vh', overflowY:'auto'}}>
-                            {this.books.map(book => (<Book key={book.title} book={book}/>))}
+                        <div className="card" style={{height:'90vh', overflowY:'auto'}}>
+                            {this.state.books.map(book => (<Book key={book.title} book={book}/>))}
                         </div>
                     </div>
                 </div>
