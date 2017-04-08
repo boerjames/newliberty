46229ef71b37bf0da7a5da5c5711d7aa5334cfd1
James Boer <boer.james@gmail.com>
2017-04-08 16:33:23 -0600

    remove some components

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import {BrowserRouter, Route} from 'react-router-dom'
 import './style/custom.scss'
 
 import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
 import Home from './components/Home.jsx'
 import Books from './components/Books.jsx'
 
@@ -15,7 +14,6 @@ render(
 			<Header/>
 			<Route exact path="/" component={Home}/>
 			<Route path="/books" component={Books}/>
-			<Footer/>
 		</div>
 	</BrowserRouter>,
 	document.getElementById('app')
