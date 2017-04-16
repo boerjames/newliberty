c47081b73505b210647adaf26665cdf4a2fd028c
James Boer <boer.james@gmail.com>
2017-04-16 16:53:42 -0600

    add hayek, helmet, port 80

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const path = require('path')
+const helmet = require('helmet')
 const express = require('express')
 const app = express()
 
+app.use(helmet())
 app.use(express.static(__dirname + '/dist'))
 app.get('*', function response(req, res) {
     res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 
-app.listen(8080, function() {
-    console.log('listening on port 8080...')
+app.listen(80, function() {
+    console.log('listening on port 80...')
 })
