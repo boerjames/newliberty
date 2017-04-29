758327e330aaa64705ec2ee5aaf5e3e6043912ab
James Boer <boer.james@gmail.com>
2017-04-28 22:47:32 -0600

    clean up

--- src/components/Arguments.jsx
+++ src/components/Arguments.jsx
@@ -8,7 +8,6 @@ class Arguments extends Component {
                     <div className="container has-text-centered">
                         <p className="title has-text-centered">A place for arguments</p>
                         <p className="has-text-centered">I think Justin wanted something like this</p>
-                        <p className="has-test-centered">Please add <a href="https://reacttraining.com/react-router/web/guides/code-splitting">code-splitting</a> to these pages or it will get too big.</p>
                     </div>
                 </div>
             </div>
